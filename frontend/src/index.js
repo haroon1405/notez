7e95cfb273c714830ac1ee2276c38d1aae596427
haroon1405 <mohammedharoonmh2000@gmail.com>
2024-02-20 21:22:27 +0400

    tailwind added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Notezz</h1>
+    <h1 className='text-3xl text-center'>Notezz</h1>
   </React.StrictMode>
 );
