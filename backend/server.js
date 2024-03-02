bc39029d796b9049b74ef770db30d4687dd79985
haroon1405 <mohammedharoonmh2000@gmail.com>
2024-03-02 11:17:28 +0400

    setup mongo atlas

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express();
+const mongoose = require("mongoose");
 const noteRoutes = require("./routes/notes.js")
 
 // Use json middleware
@@ -10,7 +11,14 @@ app.use(express.json());
 // Use Routes
 app.use("/notes",noteRoutes);
 
-// Listen on Port
-app.listen(process.env.PORT,()=>{
-    console.log(`Listening on Port-${process.env.PORT}`)
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    // Listen on Port
+    app.listen(process.env.PORT,()=>{
+        console.log(`Connect to DB & Listening on Port-${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log(err)
 })
